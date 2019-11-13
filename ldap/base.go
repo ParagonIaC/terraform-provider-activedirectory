53c1b18bbb1f3a0394ccb0412c0e1bb19262cc54
Robert Adler <radler@bethel.jw.org>
2019-11-13 13:23:39 +0100

    renew base.go

--- ldap/base.go
+++ ldap/base.go
@@ -7,10 +7,20 @@ import (
 	"gopkg.in/ldap.v3"
 )
 
-// APIInterface is the basic interface for AD API
-type APIInterface interface {
+// LDAPInterface is the basic interface for LDAP API
+type LDAPInterface interface {
 	Connect(string, string) error
 
+	// generel ldap object
+	GetObjectBy(dn string) (*LDAPObject, error)
+	SearchObject(query string) ([]*LDAPObject, error)
+
+	CreateObject(dn string, objType LDAPType, attributes map[string]string) error
+	DeleteObject(dn string) error
+
+	UpdateObject(dn string, attributes map[string]string) error
+
+	// comupter object part
 	GetComputersByLDAPFilter(string, string, []string) ([]*Computer, error)
 	GetComputerByDN(string, string, []string) (*Computer, error)
 
@@ -22,38 +32,47 @@ type APIInterface interface {
 	DeleteComputer(dn string) error
 }
 
-// API is the basic struct which should implement the APIInterface
-type API struct {
-	domain   string
-	ip       string
-	username string
-	password string
-	client   *ldap.Conn
+// LDAP is the basic struct which should implement the LDAPInterface
+type LDAP struct {
+	ldapHost     string
+	ldapPort     int
+	useTLS       bool
+	bindUser     string
+	bindPassword string
+	client       *ldap.Conn
 }
 
 // NewAPI create a AD API object
-func NewAPI(ip, domain string) (api *API) {
-	return &API{ip: ip, domain: domain}
+func NewLDAPConnection(host string, port int, useTLS bool) *LDAP {
+	return &LDAP{ldapHost: host, ldapPort port, useTLS: useTLS}
 }
 
 // Connect connects to an Active Directory server
 //	username - string
 // 	password - string
-func (api *API) Connect(username, password string) (err error) {
-	api.username = fmt.Sprintf("%s@%s", username, api.domain)
-	api.password = password
+func (api *LDAP) Connect(username, password string) (err error) {
+	api.bindUser = username
+	api.bindPassword = password
 
-	api.client, err = ldap.Dial("tcp", fmt.Sprintf("%s:%d", api.ip, 389))
+	api.client, err = ldap.Dial("tcp", fmt.Sprintf("%s:%d", api.ldapHost, api.ldapPort))
 	if err != nil {
-		log.Errorf("Connection to %s:%d failed: %s", api.ip, 389, err)
+		log.Errorf("Connection to %s:%d failed: %s", api.ldapHost, api.ldapPort, err)
 		return err
 	}
 
-	if err = api.client.Bind(api.username, api.password); err != nil {
+	if c.UseTLS {
+		if err := api.client.StartTLS(&tls.Config{InsecureSkipVerify: true}); err != nil {
+			api.client = nil
+			return err
+		}
+	}
+
+	if err = api.client.Bind(api.bindUser, api.bindPassword); err != nil {
 		log.Errorf("Authentication failed: %s", err)
+		api.client.Close()
 		return err
 	}
 
-	log.Debugf("AD connection successful for user: %s", api.username)
+	log.Debugf("AD connection successful for user: %s", api.bindUser)
 	return nil
 }

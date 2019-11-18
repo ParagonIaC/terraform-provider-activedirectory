e7e43d922233379938948bae6dc024d7bf62f4cb
Robert Adler <radler@bethel.jw.org>
2019-11-18 15:10:13 +0100

    resource_ldap_computer tests

--- ldap/provider_test.go
+++ ldap/provider_test.go
@@ -11,23 +11,22 @@ func TestProvider(t *testing.T) {
 	t.Run("Provider - Should return a valid 'schema.Provider'", func(t *testing.T) {
 		response := Provider()
 
-		assert := assert.New(t)
-		assert.NotNil(response)
-		assert.IsType(&schema.Provider{}, response)
+		assert.NotNil(t, response)
+		assert.IsType(t, &schema.Provider{}, response)
 
-		assert.Equal(schema.TypeString, response.(*schema.Provider).Schema["ldap_host"].Type)
-		assert.Equal(true, response.(*schema.Provider).Schema["ldap_host"].Required)
+		assert.Equal(t, schema.TypeString, response.(*schema.Provider).Schema["ldap_host"].Type)
+		assert.Equal(t, true, response.(*schema.Provider).Schema["ldap_host"].Required)
 
-		assert.Equal(schema.TypeInt, response.(*schema.Provider).Schema["ldap_port"].Type)
-		assert.Equal(false, response.(*schema.Provider).Schema["ldap_port"].Required)
+		assert.Equal(t, schema.TypeInt, response.(*schema.Provider).Schema["ldap_port"].Type)
+		assert.Equal(t, false, response.(*schema.Provider).Schema["ldap_port"].Required)
 
-		assert.Equal(schema.TypeBool, response.(*schema.Provider).Schema["use_tls"].Type)
-		assert.Equal(false, response.(*schema.Provider).Schema["use_tls"].Required)
+		assert.Equal(t, schema.TypeBool, response.(*schema.Provider).Schema["use_tls"].Type)
+		assert.Equal(t, false, response.(*schema.Provider).Schema["use_tls"].Required)
 
-		assert.Equal(schema.TypeString, response.(*schema.Provider).Schema["bind_user"].Type)
-		assert.Equal(true, response.(*schema.Provider).Schema["bind_user"].Required)
+		assert.Equal(t, schema.TypeString, response.(*schema.Provider).Schema["bind_user"].Type)
+		assert.Equal(t, true, response.(*schema.Provider).Schema["bind_user"].Required)
 
-		assert.Equal(schema.TypeString, response.(*schema.Provider).Schema["bind_password"].Type)
-		assert.Equal(true, response.(*schema.Provider).Schema["bind_password"].Required)
+		assert.Equal(t, schema.TypeString, response.(*schema.Provider).Schema["bind_password"].Type)
+		assert.Equal(t, true, response.(*schema.Provider).Schema["bind_password"].Required)
 	})
 }
